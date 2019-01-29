ef7f71d889dff5906f0d27dd4b562b9482a00d95
Steve Parker <steve.parker@tescobank.com>
2019-01-29 13:43:40 +0000

    adding dumb deployment output

---
+++ put_dashboard.py
@@ -0,0 +1,37 @@
+import os
+import json
+import boto3
+
+COLOR = '00ff00'
+
+
+def main():
+    client = boto3.client('cloudwatch')
+
+    env = os.environ['EnvironmentName']
+
+    response = client.put_dashboard(DashboardName=f'Demo-{env}', DashboardBody=get_body(env))
+    print(response)
+
+
+def get_body(env):
+    body = {
+        "widgets": [
+            {
+                "type": "text",
+                "x": 0,
+                "y": 0,
+                "width": 21,
+                "height": 15,
+                "properties": {
+                    "markdown": f"\n# {env}\n\n\n\n![alt text](https://htmlcolors.com/color-image/{COLOR}png)\n"
+                }
+            }
+        ]
+    }
+
+    return json.dumps(body)
+
+
+if __name__ == "__main__":
+    main()
